f418808cff29417d55ea25e9b5257721a52e8af4
leandro <carolino.leandro@gmail.com>
2020-09-14 16:24:50 +0200

    can activate added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
 import { appRoutes } from './routes';
 import { NavBarComponent } from './nav/navbar.component';
 import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-route-activator.service';
 @NgModule({
   imports: [
-
-  BrowserModule,
+BrowserModule,
         RouterModule.forRoot(appRoutes)
       ],
   declarations: [
@@ -23,9 +24,10 @@ import { CreateEventComponent } from './events/create-event.component';
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
-  providers: [EventService, ToastrService],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
